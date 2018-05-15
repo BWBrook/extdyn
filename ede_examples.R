aa31bcd6b14b710c9a28ba25f085c8370b0d0c7f
Barry Brook <barry.brook@utas.edu.au>
2018-05-15 11:33:43 +1000

    Add files via upload

---
+++ ede_examples.R
@@ -0,0 +1,72 @@
+# PE Test Data Multiple Years ##########
+year.pe <- c(1929,1930,1931,1936,1939) # physical evidence / confirmed sighting
+pe <- rep(p.vec[1],length(year.pe))
+year.ie <- c(1929,1934,1936,1942,1944,1945) # independent expert observation
+ie <- rep(p.vec[2],length(year.ie))
+year.cs <- c(1931,1936,1937,1937,1939,1942,1942,1946,1947,1947,1951,1952,1955) # controversial sighting
+cs <- rep(p.vec[3],length(year.cs)) # data set including multiple occurences of a single year
+########################################
+
+# Tachybaptus rufolavatus ##############
+year.pe <- c(1929,1960,1963,1969) # physical evidence / confirmed sighting
+pe <- rep(p.vec[1],length(year.pe))
+year.ie <- c(1972,1982,1985) # independent expert observation
+ie <- rep(p.vec[2],length(year.ie))
+year.cs <- c(1947,1970,1971,1986,1988) # controversial sighting
+cs <- rep(p.vec[3],length(year.cs))
+########################################
+
+# Pterodroma caribbaea #################
+year.pe <- c(1829,1866,1879) # physical evidence / confirmed sighting
+pe <- rep(p.vec[1],length(year.pe))
+year.ie <- c(1891) # independent expert observation
+ie <- rep(p.vec[2],length(year.ie))
+year.cs <- c(1789,1847) # controversial sighting
+cs <- rep(p.vec[3],length(year.cs))
+########################################
+
+# Ivory-billed woodpecker ##############
+year.pe <- c(1897:1902,1904:1910,1913,1914,1917,1924,1925,1932,1935,1938,1939) # physical evidence / confirmed sighting
+pe <- rep(p.vec[1],length(year.pe))
+year.ie <- c(1911,1916,1920,1921,1923,1926,1929,1930,1931,1933,1934,1936,1937,1941:1944) # independent expert observation
+ie <- rep(p.vec[2],length(year.ie))
+year.cs <- c(1946,1948:1952,1955,1958,1959,1962,1966:1969,1971:1974,1976,1981,1982,1985:1988,1999,2004:2006) # controversial
+cs <- rep(p.vec[3],length(year.cs))
+########################################
+
+# Eskimo curlew ########################
+year.pe <- c(1900:1906,1908,1911:1914,1918,1932,1962,1963) # physical evidence
+pe <- rep(p.vec[1],length(year.pe))
+year.ie <- c(1926,1929,1930,1933,1937,1939,1945,1947,1948,1950,1959:1961) # independent expert opinion
+ie <- rep(p.vec[2],length(year.ie))
+year.cs <- c(1956,1964,1968,1970,1972:1974,1976,1977,1980:1983,1985,1987,1990,1992,1996,2002,2006) # controversial
+cs <- rep(p.vec[3],length(year.cs))
+########################################
+
+# Nukup'u ##############################
+year.pe <- c(1837,1838,1888,1891:1897,1899) # physical evidence
+pe <- c(rep(p.vec[1],length(year.pe)))
+year.ie <- c(1879) # independent expert opinion
+ie <- c(rep(p.vec[2],length(year.ie)))
+year.cs <- c(1960,1961,1965,1967,1968,1973:1976,1978,1979,1981,1983,1985:1996) # controversial
+cs <- c(rep(p.vec[3],length(year.cs)))
+########################################
+
+# O'ahu 'Alauahio ######################
+year.pe <- c(1837,1888,1891:1893,1896,1897,1901:1903,1950,1968) # physical evidence
+pe <- rep(p.vec[1],length(year.pe))
+year.ie <- c(1936,1937,1939,1940,1946:1949) # independent expert opinion
+ie <- rep(p.vec[2],length(year.ie))
+year.cs <- c(1952,1955:1958,1960,1961,1964,1966,1968,1969,1972:1978,1984,1985,1989:1991,1993,1996,1997,2000:2002) # controversial
+cs <- rep(p.vec[3],length(year.cs))
+########################################
+
+# Barbary Lion ### #####################
+year.pe <- c(1911,1917,1925,1934) # high quality
+pe <- rep(p.vec[1],length(year.pe))
+year.ie <- c(1895,1900,1900,1910,1911,1911,1912,1912,1917,1920,1930,1935,1935,1942,1956) # reliable
+ie <- rep(p.vec[2],length(year.ie))
+year.cs <- c(1898,1901,1905,1920,1922,1929,1930,1930,1930,1935,1939,1943,1949) # controversial
+cs <- rep(p.vec[3],length(year.cs))
+########################################
+
